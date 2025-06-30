8bf36603bab2fc4d9cf89790051300728a32a57a
Alvin <masambu18@gmail.com>
2025-06-30 14:08:10 +0300

    Add Pharmacy-Management-System as normal folder

---
+++ Pharmacy-Management-System/PHARMACY/PHARMACY/PrintingControl/MySqlConnection_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PHARMACY.PrintingControl
+{
+    class MySqlConnection_2
+    {
+        internal void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
